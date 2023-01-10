6757130c15b986c6e1caead0148ea5707b9937b3
simlenodir <nodirsmailov0@gmail.com>
2023-01-10 11:27:04 +0300

    edede

--- src/modules/like/like.ts
+++ src/modules/like/like.ts
@@ -1,3 +1,17 @@
-export const likeGet = () => {
+import { NextFunction, Request, Response } from "express"
+import { allLikes } from "./model";
 
+export const likeGet = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const likes = await allLikes()
+        if (likes) {
+            res.json({
+                data: likes,
+                status: 200
+            })
+        }
+    } catch (error) {
+        console.log(error);
+        next(error)
+    }
 }
